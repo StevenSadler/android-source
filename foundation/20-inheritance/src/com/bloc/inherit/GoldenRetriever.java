0fd39bc01450f2ff4d7ece22c7e02c8df39e5ece
Steven Sadler <stevenjsadler@gmail.com>
2015-11-17 22:16:47 -0800

    assignment 20 completed

--- foundation/20-inheritance/src/com/bloc/inherit/GoldenRetriever.java
+++ foundation/20-inheritance/src/com/bloc/inherit/GoldenRetriever.java
@@ -7,3 +7,25 @@ package com.bloc.inherit;
  *	Golden Retrievers shrink to a smaller size after
  *	playing only 3 times.
 /************************************************/
+
+class GoldenRetriever extends Dog {
+
+	/*
+	 * play
+	 * Side-effect: 1. The GoldenRetriever loses weight
+	 *				2. Every 3 play invocations, the GoldenRetriever shrinks to a smaller size, if possible
+	 *				i.e. "large" -> "average" -> "small" -> "tiny"
+	 * @return nothing
+	 */
+	void play() {
+		setWeight(getWeight() - WEIGHT_LOST_FROM_FEEDING);
+		if (getWeight() < MINIMUM_WEIGHT) {
+			setWeight(MINIMUM_WEIGHT);
+		}
+		// Pre-increment play counter
+		if (++mPlayCounter == 3) {
+			changeSize(false);
+			mPlayCounter = 0;
+		}
+	}
+}
