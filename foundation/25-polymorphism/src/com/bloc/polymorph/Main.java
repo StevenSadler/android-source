91d96aa9b5bb0024ea4bf24c68bf0857f07fe369
Steven Sadler <stevenjsadler@gmail.com>
2015-11-20 00:56:49 -0800

    assignment 25 polymorphism with hashmap

--- foundation/25-polymorphism/src/com/bloc/polymorph/Main.java
+++ foundation/25-polymorphism/src/com/bloc/polymorph/Main.java
@@ -1,53 +1,40 @@
 package com.bloc.polymorph;
 
+import java.util.HashMap;
 import com.bloc.polymorph.pets.*;
 
 public class Main extends Object {
 
 	public static void main(String [] args) {
-
-		Pet dog = new Dog();
-		Pet cat = new Cat();
-		Pet bird = new Bird();
-		Pet snake = new Snake();
-		Pet tarantula = new Tarantula();
+		
+		HashMap<String, Pet> pets = new HashMap<String, Pet>();
+		pets.put("dog", new Dog());
+		pets.put("cat", new Cat());
+		pets.put("bird", new Bird());
+		pets.put("snake", new Snake());
+		pets.put("tarantula", new Tarantula());
 
 		/************************************************
 	 	 *	ASSIGNMENT:
 	 	 *	Replace the operations below by employing polymorphism
 		/************************************************/
 
-		dog.feed();
-		dog.wash();
-		dog.exercise();
-
-		cat.feed();
-		cat.wash();
-		cat.exercise();
-
-		bird.feed();
-		bird.wash();
-		bird.exercise();
-
-		snake.feed();
-		snake.wash();
-		snake.exercise();
-
-		tarantula.feed();
-		tarantula.wash();
-		tarantula.exercise();
+		for (Pet pet : pets.values()) {
+			pet.feed();
+			pet.wash();
+			pet.exercise();
+		}
 
 		/************************************************
 	 	 *	ASSIGNMENT:
 	 	 *	Replace the operations above by employing polymorphism
 		/************************************************/
 
-		assert dog.isFed() && dog.isWashed() && dog.isExercised() : "Your dog needs a little more attention";
-		assert cat.isFed() && cat.isWashed() && cat.isExercised() : "Your cat needs a little more attention";
-		assert bird.isFed() && bird.isWashed() && bird.isExercised() : "Your bird needs a little more attention";
-		assert snake.isFed() && snake.isWashed() && snake.isExercised() : "Your snake needs a little more attention";
-		assert tarantula.isFed() && tarantula.isWashed() && tarantula.isExercised() : "Your tarantula needs a little more attention";
-		
+		for (String name : pets.keySet()) {
+			Pet pet = pets.get(name);
+			assert pet.isFed() && pet.isWashed() && pet.isExercised() : "Your " + name + " needs a little more attention";
+		}
+
 		System.out.println("/************************/");
 		System.out.println("/*                      */");
 		System.out.println("/*                      */");
