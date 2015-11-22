2d669d4d469f25cd5b28eb672aac7f9d3d84ed53
Steven Sadler <stevenjsadler@gmail.com>
2015-11-21 19:28:55 -0800

    assignment 31 complete

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,6 +1,14 @@
 package com.bloc.threads;
 
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
+
+	private BufferedImage mBufferedImage;
+	private boolean mOpenWhenCompleted;
 
 	/*
 	 * ImageGetter
@@ -12,6 +20,22 @@ public class ImageGetter extends Thread {
 	 * @param openWhenCompleted if `true`, the image will be opened
 	 *		  as soon it is downloaded, `false` otherwise
 	 */
+	public ImageGetter(String url, boolean openWhenCompleted) {
+		try {
+			mOpenWhenCompleted = openWhenCompleted;
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			}
+			mBufferedImage = ImageIO.read(new URL(url));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 	/************************************************
  	 *	ASSIGNMENT:
  	 *	Implement this constructor for ImageGetter.
@@ -21,6 +45,23 @@ public class ImageGetter extends Thread {
 
 	@Override
 	public void run() {
+		if (mOpenWhenCompleted) {
+			try {
+				File outputfile = new File("google_logo.png");
+				ImageIO.write(mBufferedImage, "png", outputfile);
+				if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+					Runtime.getRuntime().exec("open google_logo.png");
+				} else {
+					Runtime.getRuntime().exec("google_logo.png");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
 		/************************************************
  		 *	ASSIGNMENT:
  		 *	Perform the work found in `Main.java` in this
